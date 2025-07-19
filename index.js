82ffbba3bfc1e8669cb70c6184d42a1947c4900c
Shreesh <shreeshsriv@outlook.com>
2025-07-18 23:45:55 -0600

    added routes for cat and now need to test it

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use('/cats', catsRouter);
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Geeks!</h1><p>This is your simple Express server.</p>');
 });
--- src/routes/cats.js
+++ src/routes/cats.js
@@ -9,6 +9,73 @@ router.get('/', (req, res) => {
     res.json(cats);
 });
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const cat = db.prepare('SELECT * FROM cats WHERE id = ?').get(id);
 
+    if (!cat) {
+        return res.status(404).json({ error: 'Cat not found' });
+    }
+    res.json(cat);
+});
+
+router.post('/', (req, res) => {
+    const { name, color, description, photoURL } = req.body;
+    if (!name || !color) {
+        return res.status(400).json({ error: 'Name and color are required'});
+    
+    }
+
+    const stmt = db.prepare( `
+        INSERT INTO cats (name, color, description, photoURL)
+        VALUES (?, ?, ?, ?)
+    `);
+
+    const info = stmt.run(name, color, description || '', photoURL || '' );
+
+    const newCat = db.prepare('SELECT * FROM cats WHERE id = ?').get(info.lastInsertRowid);
+    res.status (201).json(newCat);
+         
+    
+});
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const{ name, color, description, photoURL } = req.body;
+
+    // Check if the cat exists
+    const cat = db.prepare('SELECT * FROM cats  WHERE id = ?').get(id);
+    if (!cat) {
+        return res.status(404).json ({ error: 'Cat not found' });
+
+    }
+
+    // Update the cat
+    const stmt = db.prepare(`
+        UPDATE cats
+        SET name = ?, color = ?, description = ?, photoURL = ?
+        WHERE id = ?
+    `);
+    stmt.run (name || cat.name, color || cat.color, description || cat.description, photoURL || cat.photoURL, id);
+
+    const updateCat = db.prepare (
+        'SELECT * FROM cats WHERE id = ?'
+    );
+    const updatedCat = updateCat.get(id);
+    res.json(updatedCat);
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    const cat = db.prepare('SELECT * FROM cats WHERE id = ?').get(id);
+    if (!cat) {
+        return res.status(404).json ({ error: 'Cat not found' });
+    }
+
+    db.prepare (`DELETE FROM cats WHERE id = ?`).run(id);
+
+    res.json({ message: 'Cat deleted successfully'});
+    })
 
 module.exports = router;
