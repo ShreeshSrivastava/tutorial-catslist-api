eea3136e1b6292000afd371275aba06d54e3bff8
Shreesh <shreeshsriv@outlook.com>
2025-07-24 17:50:56 -0600

    adding tests

--- index.js
+++ index.js
@@ -2,16 +2,7 @@ const express = require('express');
 const catsRouter = require('./src/routes/cats');
 
 const app = express();
-const PORT = 3000;
-
 app.use(express.json());
-
 app.use('/cats', catsRouter);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, Geeks!</h1><p>This is your simple Express server.</p>');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,8 @@
+// server.js
+
+const app = require ('./index');
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
