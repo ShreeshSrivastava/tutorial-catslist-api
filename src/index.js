4507e0d6af2684ff46909d444f608c6662bec8ea
Shreesh <shreeshsriv@outlook.com>
2025-07-29 13:47:52 -0600

    making a frontend with react and typescript

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const catsRouter = require('./routes/cats');
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); //middleware to parse JSON bodies
 app.use('/cats', catsRouter);
 
 export default app;
